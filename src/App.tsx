4727ac75b7e19c2255a5eb8e22e47888c36da039
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-14 15:04:21 -0600

    Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter } from "react-router";
 import Navbar from "./components/Navbar/Navbar";
 import MyRoutes from "./routes/Routes";
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <main>
-          <MyRoutes />
-        </main>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <main>
+            <MyRoutes />
+          </main>
+        </BrowserRouter>
+      </AuthProvider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
