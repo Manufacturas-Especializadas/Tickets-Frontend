f9418e5ededda89dca86f9c10188e676b84b0aca
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-14 10:08:45 -0600

    Codigo formatado

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,21 +1,23 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-
 if (!API_BASE_URL) {
-    throw new Error("API base URL is not defined in environment variables")
+  throw new Error("API base URL is not defined in environment variables");
 }
 
 export const API_CONFIG = {
-    baseUrl: API_BASE_URL,
-    endpoints: {
-        ticketForm: {
-            tickets: '/api/TicketForm/GetTickets',
-            search: '/api/TicketForm/SearchTicketByName',
-            categories: '/api/TicketForm/GetCategories',
-            report: "/api/TicketForm/DownloadReport",
-            register: '/api/TicketForm/RegisterTicket',
-            update: '/api/TicketForm/Update/',
-            delete: '/api/TicketForm/Delete/'
-        },
+  baseUrl: API_BASE_URL,
+  endpoints: {
+    auth: {
+      login: "/api/Auth/Login",
     },
+    ticketForm: {
+      tickets: "/api/TicketForm/GetTickets",
+      search: "/api/TicketForm/SearchTicketByName",
+      categories: "/api/TicketForm/GetCategories",
+      report: "/api/TicketForm/DownloadReport",
+      register: "/api/TicketForm/RegisterTicket",
+      update: "/api/TicketForm/Update/",
+      delete: "/api/TicketForm/Delete/",
+    },
+  },
 } as const;
