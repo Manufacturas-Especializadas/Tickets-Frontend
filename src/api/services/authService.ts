a73aff3675ee59525c9b931c9cc6ff16c8e80fc6
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-14 15:04:53 -0600

    Servicio con los metodos de login y logeo

--- src/api/services/authService.ts
+++ src/api/services/authService.ts
@@ -1,3 +1,17 @@
-class AuthService {}
+import { API_CONFIG } from "../../config/api";
+import { apiClient } from "../client";
+
+class AuthService {
+  private loginEndpoint = API_CONFIG.endpoints.auth.login;
+  private logoutEndpoint = API_CONFIG.endpoints.auth.logout;
+
+  async login(payRollNumber: number, password: string): Promise<any> {
+    return apiClient.post(this.loginEndpoint, { payRollNumber, password });
+  }
+
+  async logout(): Promise<void> {
+    await apiClient.post(this.logoutEndpoint, {});
+  }
+}
 
 export const authService = new AuthService();
